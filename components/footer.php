7643b733cf7aacc97fc5f54155e5e7ff9baa9dfa
NikoPoi <themobiliowow@gmail.com>
2023-02-10 07:12:41 +0700

    Init

---
+++ components/footer.php
@@ -0,0 +1,47 @@
+<section class="footerspp pb-4">
+            <div class="container d-lg-flex">
+                <div class="row mb-3 mt-5 mr-auto justify-content-center">
+                    <div class="row mb-2">
+                        <span class="col h6">PERUSAHAAN</span>
+                        <span class="col h6">KEMITRAAN</span>
+                        <span class="col h6">MENTOR KAMI</span>
+                    </div>
+                    <div class="row mb-2">
+                        <a href="./index.html" class="col-4 text-reset">Tentang</a>
+                        <a href="https://wa.me/+6281214271724?text=Hallo%20*Admin%20jasasaerviceac.id*%0A%0ANama%20Saya%20%20%20%20%20%20%20%3A%0ADomisili%20%20%20%20%20%20%20%20%20%20%20%20%20%3A%0AAlamat%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%3A%0ANo%20Handphone%20%3A%0AEmail%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%3A%0A%0ASaya%20ingin%20daftar%20sebagai%20*Mitra%20di%20jasaserviceac.id*%20untuk%20area%3A%20%0A" class="col-4 text-reset" target="_blank">Daftar Kemitraan</a>
+                        <a href="./incubatee.html" class="col-4 text-reset">Inkubator</a>
+                    </div>
+                    <div class="row mb-2">
+                        <a href="./contact-us.html" class="col-4 text-reset">Kontak</a>
+                        <a href="https://docs.google.com/document/d/1uVdvKbm9c64XUXKicYiBDDwkheJ2vx7AEc7dq31kE8w/edit?usp=sharing" class="col-4 text-reset">Syarat Kemitraan</a>
+                        <a href="./association.html" class="col-4 text-reset">Asosiasi</a>
+                    </div>
+                    <div class="row mb-2">
+                        <a href="./career.html" class="col-4 text-reset">Karir</a>
+                        <a href="https://wa.me/+6281214271724?text=Hallo%20*Admin%20jasaserviceac.id*%20saya%20ingin%20mengikuti%20program%20afiliasi%20dari%20jasaserviceac.id%2C%20dibawah%20ini%20adalah%20data%20saya%3A%0ANama%20%20%20%20%20%20%20%20%20%3A%0APekerjaan%20%20%3A%0ADomisili%20%20%20%20%20%3A%20%20%0AEmail%20%20%20%20%20%20%20%20%20%20%3A%0A%0ABagaimana%20caranya%20kak%3F" class="col-4 text-reset" target="_blank">Program Afiliasi</a>
+                    </div>
+                </div>
+                <div>
+                    <div class="text-center">
+                        <img class="img-fluid mb-3 mt-3" src="https://e-ujian.id/wp-content/uploads/2021/04/cropped-Logo_white.png" alt="Tidak Ada Data" >
+                    </div>
+                    <div class="row mb-3">
+                        <span class="h6">Misi kami adalah membantu pengguna AC mendapatkan pelayanan jasa service AC yang jujur dan profesional.</span>
+                    </div>
+                    <div class="row mb-3">
+                        <span class="h6">HEADQUARTERS</span>
+                        <span class="w-100 h6">Komplek Griya Bandung Asri 3 blok N7 no 28 Bandung</span>
+                    </div>
+                    <div class="row mb-3">
+                        <span class="h6">BRANCH OFFICE</span>
+                        <span class="w-100 h6">Jl. Tikukur No. 13 Coblong Kota Bandung, Jawa Barat 40133</span>
+                    </div>
+                    <div class="row">
+                        <span class="h6">Email : halo@jasaserviceac.id</span>
+                        <span class="w-100 h6">Whatapp : 0812-1427-1724</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <script src="assets/vendors/js/bootstrap.bundle.js"></script>
---
+++ components/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg bg-transparent">
+            <div class="container">
+                <a class="navbar-brand" href="./index.html"><img src="https://e-ujian.id/wp-content/uploads/2021/04/cropped-Logo_white.png" alt="Logo"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="m-auto"></div>
+                    <ul class="navbar-nav d-flex mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link h6 text-dark" aria-current="page" href="./index.php">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link h6 text-dark" href="./career.html">Kontak</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle h6 text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Pembayaran
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Cek Tagihan</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="./input.php">Pembayaran SPP</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
